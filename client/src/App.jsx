d05649ab3319b74f1e0c8fc00d2ad0d74d5765d0
FranciscoSalanova <fsalanova.16@gmail.com>
2023-09-26 12:39:26 -0300

    fetching and initial rendering of images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useId, useState } from 'react'
+
 function App() {
-  return <></>
+  const [photos, setPhotos] = useState([])
+  const id = useId()
+
+  useEffect(() => {
+    fetch('http://localhost:3000/photos?_page=1&_limit=10')
+      .then((res) => res.json())
+      .then(setPhotos)
+  }, [])
+
+  return (
+    <div className="grid">
+      {photos.map((photo) => (
+        <img src={photo.url} key={`${id}-${photo.id}`} />
+      ))}
+    </div>
+  )
 }
 
 export default App

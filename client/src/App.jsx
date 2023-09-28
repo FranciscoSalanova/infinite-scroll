c795d6a9db9babdcb8f1986c43cf45ba9e4e567c
Francisco Salanova <fsalanova.16@gmail.com>
2023-09-28 10:57:46 -0300

    completing infinite scrolling with lazy loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,76 @@
-import { useEffect, useId, useState } from 'react'
+import { useCallback, useEffect, useId, useRef, useState } from "react"
+import { parseLinkHeader } from "./parseLinkHeader"
+
+const LIMIT = 10
 
 function App() {
   const [photos, setPhotos] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   const id = useId()
+  const nextPhotoUrlRef = useRef()
 
-  useEffect(() => {
-    fetch('http://localhost:3000/photos?_page=1&_limit=10')
-      .then((res) => res.json())
-      .then(setPhotos)
+  async function fetchPhotos(url, { overwrite = false } = {}) {
+    setIsLoading(true)
+    try {
+      const res = await fetch(url)
+      nextPhotoUrlRef.current = parseLinkHeader(res.headers.get("Link")).next
+      const photos = await res.json()
+
+      if (overwrite) {
+        setPhotos(photos)
+      } else {
+        setPhotos((prevPhotos) => [...prevPhotos, ...photos])
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const imageRef = useCallback((image) => {
+    if (image == null || nextPhotoUrlRef == null) return
+
+    // inicializamos el observador para observar el último elemento img visible hasta el momento
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        fetchPhotos(nextPhotoUrlRef.current)
+        observer.unobserve(image)
+      }
+    })
+
+    observer.observe(image)
   }, [])
 
+  useEffect(() => {
+    fetchPhotos(
+      `http://localhost:3000/photos-short-list?_page=1&_limit=${LIMIT}`,
+      {
+        overwrite: true,
+      }
+    )
+  }, [])
+
+  // las imágenes no van a cargarse hasta que se complete el fetch
+  // mientras esto último se cumple, isLoading va a estar en 'true' y se visualizará el array de divs skeleton
   return (
     <div className="grid">
-      {photos.map((photo) => (
-        <img src={photo.url} key={`${id}-${photo.id}`} />
+      {photos.map((photo, index) => (
+        <img
+          key={`${id}-${photo.id}`}
+          src={photo.url}
+          alt={photo.title}
+          ref={index === photos.length - 1 ? imageRef : undefined}
+        />
       ))}
+      {isLoading &&
+        Array.from({ length: LIMIT }, (_, index) => index).map((n) => {
+          return (
+            <div key={n} className="skeleton">
+              Loading...
+            </div>
+          )
+        })}
     </div>
   )
 }
--- client/src/parseLinkHeader.js
+++ client/src/parseLinkHeader.js
@@ -3,12 +3,21 @@
 */
 export function parseLinkHeader(linkHeader) {
   if (!linkHeader) return {}
+
   const links = linkHeader.split(",")
   const parsedLinks = {}
-  links.forEach(link => {
+  links.forEach((link) => {
     const url = link.match(/<(.*)>/)[1]
     const rel = link.match(/rel="(.*)"/)[1]
     parsedLinks[rel] = url
   })
+
   return parsedLinks
 }
+
+// Ejemplo del objeto 'parsedLinks' para un fetch a 'http://localhost:3000/photos?_page=1&_limit=20':
+// {
+//   'first': 'http://localhost:3000/photos?_page=1&_limit=10',
+//   'next': 'http://localhost:3000/photos?_page=2&_limit=10',
+//   'last': 'http://localhost:3000/photos?_page=500&_limit=10'
+// }
